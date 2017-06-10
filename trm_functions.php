7a0700186bbfe8fdda789f03e5a55e6019bedd41
joe <joefritsch@gmail.com>
2017-06-10 13:45:01 +0200

    trm_functions.php: git infos commented

--- trm_functions.php
+++ trm_functions.php
@@ -16,6 +16,10 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       trm_functions
+ *
+ * github:              https://github.com/jfritsch123/trm_functions
+ * active branch:       admin-ajax
+
  */
 
 // If this file is called directly, abort.
