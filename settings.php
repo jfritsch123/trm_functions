c3fa09d766d614f082c0ad256d48518a36021b91
joefritsch <jf@3mo.at>
2019-12-07 13:37:41 +0100

    Deleted not used folders and files

--- settings.php
+++ settings.php
@@ -8,32 +8,5 @@
 // trm_functions
 include_once PLUGIN_DIR_PATH.'/trm/trm_functions/settings.php';
 
-// trm_gallery
-//include_once PLUGIN_DIR_PATH.'/trm/trm_gallery/settings.php';
-
-// masonry, imagesloaded
-#include_once PLUGIN_DIR_PATH.'/vendor/masonry/settings.php';
-
-// photoswipe
-#include_once PLUGIN_DIR_PATH.'/vendor/photoswipe/settings.php';
-
-// foundation
-#include_once PLUGIN_DIR_PATH.'/vendor/foundation/settings.php';
-
-// bootstrap
-#include_once PLUGIN_DIR_PATH.'/trm/trm_bootstrap/settings.php';
-
-// date extensions
-#include_once PLUGIN_DIR_PATH.'/trm/trm_date_extensions/settings.php';
-
 // trm ajax
 //include_once PLUGIN_DIR_PATH.'/trm/trm_ajax/settings.php';
-
-// cf7 extensions
-#include_once PLUGIN_DIR_PATH.'/trm/trm_cf7_extensions/settings.php';
-
-// trm_tinymce
-#include_once PLUGIN_DIR_PATH.'/trm/trm_tinymce/settings.php';
-
-// trm_menu_schedule
-#include_once PLUGIN_DIR_PATH.'/trm/trm_menu_schedule/settings.php';
