9c167816a88da9dcbca568778f11884a70c26b46
joefritsch <jf@3mo.at>
2019-12-07 13:28:20 +0100

    Deleted not used folders and files

--- trm/trm_date_extensions/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * trm_functions
- * settings
- *
- */
-
-include_once PLUGIN_DIR_PATH.'/trm/trm_date_extensions/acf_date_fieldgroup.php';
-include_once PLUGIN_DIR_PATH.'/trm/trm_date_extensions/acf_query_posts.php';
